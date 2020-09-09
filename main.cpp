318ddeac964f3eb0a6d62950e689745282d2fe63
竹杰 <zhujie.zzh@alibaba-inc.com>
2020-09-09 11:28:28 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 /*
  * @Author       : Zihao Zhao
- * @E-mail       : zhujie.zzh@alibaba-inc.com
- * @Company      : Alibaba-inc
+ * @E-mail       : 18212020051@fudan.edu.cn
+ * @Company      : Fudan University iBiCAS
  * @Date         : 2020-09-09 11:14:55
  * @LastEditors  : Zihao Zhao
- * @LastEditTime : 2020-09-09 11:15:24
+ * @LastEditTime : 2020-09-09 11:25:51
  * @FilePath     : /data-layout-transformer/main.cpp
  * @Description  : 
  */
